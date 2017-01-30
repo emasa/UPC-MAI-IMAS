7398f6ef23ea89cf4b5316e05fd7c22ab4cbeb87
Daniel Zapata <daniel.natanael.garcia@est.fib.upc.edu>
2017-01-30 15:17:23 +0100

    Contract Net Dummy

--- mas-skeleton/src/cat/urv/imas/agent/CoordinatorAgent.java
+++ mas-skeleton/src/cat/urv/imas/agent/CoordinatorAgent.java
@@ -20,11 +20,23 @@ package cat.urv.imas.agent;
 import cat.urv.imas.onthology.GameSettings;
 import cat.urv.imas.behaviour.coordinator.RequesterBehaviour;
 import cat.urv.imas.onthology.MessageContent;
+import cat.urv.imas.map.BuildingCell;
+import cat.urv.imas.map.SettableBuildingCell;
+import cat.urv.imas.onthology.GarbageType;
+
 import jade.core.*;
 import jade.domain.*;
+import jade.proto.ContractNetInitiator;
 import jade.domain.FIPAAgentManagement.*;
 import jade.domain.FIPANames.InteractionProtocol;
 import jade.lang.acl.*;
+import java.io.IOException;
+
+import java.util.Date;
+import java.util.Vector;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * The main Coordinator agent. 
@@ -61,13 +73,13 @@ public class CoordinatorAgent extends ImasAgent {
         /* ********************************************************************/
 
         // Register the agent to the DF
-        ServiceDescription sd1 = new ServiceDescription();
-        sd1.setType(AgentType.COORDINATOR.toString());
-        sd1.setName(getLocalName());
-        sd1.setOwnership(OWNER);
+        ServiceDescription ca = new ServiceDescription();
+        ca.setType(AgentType.COORDINATOR.toString());
+        ca.setName(getLocalName());
+        ca.setOwnership(OWNER);
         
         DFAgentDescription dfd = new DFAgentDescription();
-        dfd.addServices(sd1);
+        dfd.addServices(ca);
         dfd.setName(getAID());
         try {
             DFService.register(this, dfd);
@@ -95,12 +107,77 @@ public class CoordinatorAgent extends ImasAgent {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         //we add a behaviour that sends the message and waits for an answer
         this.addBehaviour(new RequesterBehaviour(this, initialRequest));
-
         // setup finished. When we receive the last inform, the agent itself will add
         // a behaviour to send/receive actions
+        
+        /* ********************************************************************/
+        // contract net system
+        ServiceDescription searchHC = new ServiceDescription();     
+        searchHC.setType(AgentType.HARVESTER_COORDINATOR.toString());
+        this.systemAgent = UtilsAgents.searchAgent(this, searchHC);    
+        
+        ////////////// Dummy SettableBuildingCell
+        SettableBuildingCell celda = new SettableBuildingCell(0, 1);
+        celda.setGarbage(GarbageType.PAPER, 2);
+        String content = celda.getMapMessage()+" in R"+celda.getRow()+" C"+celda.getCol();        
+        ///////////////
+        
+        // Fill the CFP message
+        ACLMessage contract = new ACLMessage(ACLMessage.CFP);
+        contract.addReceiver(this.systemAgent);
+        contract.setProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET);
+        // We want to receive a reply in 10 secs
+        contract.setReplyByDate(new Date(System.currentTimeMillis() + 10000));
+        contract.setContent(content);                             
+        
+        try {
+            contract.setContentObject(celda);
+        } catch (IOException ex) {
+            Logger.getLogger(CoordinatorAgent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        log("ContractNet Started");
+        System.out.println("1. Coordinator Agent sent a ContractNet to collect "+content);
+        
+        this.addBehaviour(new ContractNetInitiator(this, contract) {			
+            protected void handlePropose(ACLMessage propose, Vector v) {
+                // Receive Proposal
+                System.out.println("3. Agent "+propose.getSender().getName()+" Proposed a Coalition to collect: "+propose.getContent());             
+            }
+
+            protected void handleRefuse(ACLMessage refuse) {
+                System.out.println("3. Agent "+refuse.getSender().getName()+" refused");
+            }
+
+            protected void handleFailure(ACLMessage failure) {
+                if (failure.getSender().equals(myAgent.getAMS())) {
+                    // FAILURE notification from the JADE runtime: the receiver
+                    // does not exist
+                    System.out.println("Responder does not exist");
+                }
+                else {
+                    System.out.println("Agent "+failure.getSender().getName()+" failed");
+                }
+            }
+
+            protected void handleAllResponses(Vector responses, Vector acceptances) {
+                // Accept Proposal. CA always accepts proposal
+                Enumeration e = responses.elements();
+                while (e.hasMoreElements()) {
+                    ACLMessage proposal = (ACLMessage) e.nextElement();                    
+                    ACLMessage accept = proposal.createReply();
+                    accept.setPerformative(ACLMessage.ACCEPT_PROPOSAL);
+                    acceptances.addElement(accept);
+                    accept.setContent(proposal.getContent()); 
+                    System.out.println("4. "+getLocalName()+" Accepted proposal: "+proposal.getContent());
+                }             
+            }
+                        
+            protected void handleInform(ACLMessage inform) {
+                System.out.println("8. "+inform.getSender().getName()+" successfully performed: "+inform.getContent());
+            }
+        });
     }
 
     /**
--- mas-skeleton/src/cat/urv/imas/agent/HarvesterCoordinatorAgent.java
+++ mas-skeleton/src/cat/urv/imas/agent/HarvesterCoordinatorAgent.java
@@ -5,9 +5,28 @@
  */
 package cat.urv.imas.agent;
 
+import static cat.urv.imas.agent.ImasAgent.OWNER;
+import cat.urv.imas.map.SettableBuildingCell;
+import cat.urv.imas.onthology.MessageWrapper;
+
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.UnreadableException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
- * @author Dario
+ * @author Daniel
  */
 public class HarvesterCoordinatorAgent extends ImasAgent{
      /**
@@ -19,8 +38,94 @@ public class HarvesterCoordinatorAgent extends ImasAgent{
 
     @Override
     protected void setup() {
-        log("Creatad new Harvester Coordinator: " + getLocalName());
+        /* ** Very Important Line (VIL) ***************************************/
+        this.setEnabledO2ACommunication(true, 1);
+        /* ********************************************************************/
+
+        // Register the agent to the DF
+        ServiceDescription hca = new ServiceDescription();
+        hca.setType(AgentType.HARVESTER_COORDINATOR.toString());
+        hca.setName(getLocalName());
+        hca.setOwnership(OWNER);
         
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.addServices(hca);
+        dfd.setName(getAID());
+        try {
+            DFService.register(this, dfd);
+            log("Registered to the DF");
+        } catch (FIPAException e) {
+            System.err.println(getLocalName() + " registration with DF unsucceeded. Reason: " + e.getMessage());
+            doDelete();
+        }        
+        log("Creatad new Harvester Coordinator: " + getLocalName());
+
+        /* ********************************************************************/    
+        // contract net system        
+        System.out.println("Agent "+getLocalName()+" waiting for CFP...");
+  	MessageTemplate template = MessageTemplate.and(
+            MessageTemplate.MatchProtocol(FIPANames.InteractionProtocol.FIPA_CONTRACT_NET),
+            MessageTemplate.MatchPerformative(ACLMessage.CFP) );
+  		
+	addBehaviour(new ContractNetResponder(this, template) {
+            protected ACLMessage prepareResponse(ACLMessage cfp) throws NotUnderstoodException, RefuseException {
+                SettableBuildingCell proposal = null;
+                try {
+                    // Call evaluateAction to convert cfp to SettableBuildingCell
+                    proposal = evaluateAction(cfp);
+                    System.out.println("2. "+getLocalName()+": CFP Received from "+cfp.getSender().getName()+". Action is to collect: "+proposal.getMapMessage());
+                } catch (UnreadableException ex) {
+                    Logger.getLogger(HarvesterCoordinatorAgent.class.getName()).log(Level.SEVERE, null, ex);
+                    System.out.println("2. "+getLocalName()+": Refuse");
+                    throw new RefuseException("evaluation-failed");                    
+                }
+                // Provide a proposal: HC always accepts proposal           
+                ACLMessage propose = cfp.createReply();
+                propose.setPerformative(ACLMessage.PROPOSE);
+                try {
+                    propose.setContentObject(proposal);
+                } catch (IOException ex) {
+                    Logger.getLogger(CoordinatorAgent.class.getName()).log(Level.SEVERE, null, ex);
+                }                
+                propose.setContent(String.valueOf(proposal));                 
+                return propose;              
+            }
+			
+            protected ACLMessage prepareResultNotification(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException {
+                System.out.println("5. "+getLocalName()+": Proposal: "+accept.getContent()+" accepted");
+		if (performAction()) {
+                    System.out.println("7. "+getLocalName()+": Action successfully performed");
+                    ACLMessage inform = accept.createReply();
+                    inform.setPerformative(ACLMessage.INFORM);
+                    inform.setContent(accept.getContent());
+                    return inform;
+		}
+		else {
+                    System.out.println("7. "+getLocalName()+": Action execution failed");
+                    throw new FailureException("unexpected-error");
+		}	
+            }
+			
+            protected void handleRejectProposal(ACLMessage reject) {
+                System.out.println("5. "+getLocalName()+": Proposal:"+reject.getContent()+" rejected");
+            }
+        } );
+    }
+  
+    private SettableBuildingCell evaluateAction(ACLMessage contract) throws UnreadableException {
+  	// Convert ACLMessage to SettableBuildingCell
+        SettableBuildingCell proposal = (SettableBuildingCell) contract.getContentObject();
+
+  	return proposal;
+    }
+  
+    private boolean performAction() { 
+        System.out.println("6. "+getLocalName()+" formed a coalition");
+        
+  	// Call coalition to collect garbage
+        MessageWrapper messageCoalition = new MessageWrapper.setObject();
+        // coalition(cellBuilding)
+  	return true;
     }
 
 }
